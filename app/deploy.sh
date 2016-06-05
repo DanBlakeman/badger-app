185f4c4e92a4ed0131653132df965d0775655ec8
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2016-06-05 13:38:41 +0100

    Add deploy script triggered by circle

---
+++ app/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "pulling new changes from GH"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'cd badger-app && git pull'
+
+echo "rebuilding web container"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose build web'
+
+echo "restarting web container"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose up --no-deps -d web'
+
+echo "success!"
+
+exit 0
