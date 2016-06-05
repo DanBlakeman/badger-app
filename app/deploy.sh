3adc3fc76e5991e0495ed066c36cd2448436f286
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2016-06-05 14:00:46 +0100

    Change deploy script permissions

--- app/deploy.sh
+++ app/deploy.sh
@@ -4,10 +4,10 @@ echo "pulling new changes from GH"
 ssh $DEPLOY_USER@$DEPLOY_HOST 'cd badger-app && git pull'
 
 echo "rebuilding web container"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose build web'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'cd badger-app && docker-compose build web'
 
 echo "restarting web container"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose up -f docker-compose.yml -f docker-compose.production.yml --no-deps -d web '
+ssh $DEPLOY_USER@$DEPLOY_HOST 'cd badger-app && docker-compose -f docker-compose.yml -f production.yml up --no-deps -d web'
 
 echo "success!"
 
